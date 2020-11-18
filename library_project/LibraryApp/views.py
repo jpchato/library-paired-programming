78ca5db762d32d1e9fa3c9edead4ae23765b4c05
jpchato <jpchato@gmail.com>
2020-11-17 18:39:29 -0800

    updated model and files to display model properties on home.html

--- library_project/LibraryApp/views.py
+++ library_project/LibraryApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Author, Book
 
 # Create your views here.
+
+def home(request):
+    books = Book.objects.all()
+    context = {
+        'books' : books,
+    }
+    return render(request, 'LibraryApp/home.html', context)
+
