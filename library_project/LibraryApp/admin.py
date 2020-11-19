fe0e6352984b55fc0f36248db924a16f4682361f
jpchato <jpchato@gmail.com>
2020-11-18 21:03:01 -0800

    updated model with foreign keys and dunder str

--- library_project/LibraryApp/admin.py
+++ library_project/LibraryApp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Author, Book, ActiveUsers
+from .models import Author, Book, ActiveUser
 
 from . import models
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Book)
-admin.site.register(ActiveUsers)
+admin.site.register(ActiveUser)
