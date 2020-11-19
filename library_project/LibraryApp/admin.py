a3747090f2bd9d2b36b8ae6273bb8be5746840dd
SPEARTECH <tyler_retzlaff@yahoo.com>
2020-11-18 23:34:54 -0500

    updated ActiveUsers model

--- library_project/LibraryApp/admin.py
+++ library_project/LibraryApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Author, Book
+from .models import Author, Book, ActiveUsers
 
 from . import models
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Book)
+admin.site.register(ActiveUsers)
